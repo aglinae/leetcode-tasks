f3527b061706c025e75d35471fabff62a92d41f2
Sultan <sbaratahunov@gmail.com>
2023-09-15 21:36:51 +0600

    feat: timeout cancellation is done

--- TimeoutCancellation/leetcode.js
+++ TimeoutCancellation/leetcode.js
@@ -3,17 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-  let result = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i + 1]) {
-      result = nums[i] + nums[i + 1];
-      if (result === target) {
-        return result;
-      }
+var cancellable = function (fn, args, t) {
+  let timeoutId = null;
+  function cancelFn() {
+    if (timeoutID !== null) {
+      clearTimeout(timeoutId);
     }
   }
+  timeoutId = setTimeout(() => {
+    fn(...args);
+    timeoutId = null;
+  }, t);
+  return cancelFn;
 };
-(nums = [2, 7, 11, 15]), (target = 9);
-
-twoSum(nums, target);
